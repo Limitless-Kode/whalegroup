7a5d33b0d1f4b438671744d1fcdff3aecdec10ea
unknown <limitless.claver@gmail.com>
2021-03-17 18:58:07 +0000

    Navigation Drawer implemented

--- src/app/components/navigation.component.jsx
+++ src/app/components/navigation.component.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars,FaTimes } from "react-icons/fa";
 // import useScript from '../../hooks/ImportScript';
 
 export default function Navigation(){
-   // useScript("../../assets/js/jquery.min.js");
-   
+    const [drawerOpenState, setDrawerOpenState] = useState(false);
+    
     return(
         <div className="navigation">
             <div className="logo">
                 <img src={require("../../assets/images/logo.png")} alt="The Whale Group Logo" height="45px"/>
             </div>
-            <div className="navigation__links">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/instructors">Instructors</Link>
-                <Link className="btn medium" to="/apply">Apply Now</Link>
+            <div id="menu" role="button" onClick={()=> setDrawerOpenState(true)}>
+                <FaBars />
+            </div>
+            <div className={`navigation__links ${drawerOpenState ? "show" : ""}`} >
+                <div id="close" role="button" onClick={()=> setDrawerOpenState(false)}>
+                    <FaTimes />
+                </div>
+                <Link to="/" onClick={()=> setDrawerOpenState(false)}>Home</Link>
+                <Link to="/about" onClick={()=> setDrawerOpenState(false)}>About</Link>
+                <Link to="/gallery" onClick={()=> setDrawerOpenState(false)}>Gallery</Link>
+                <Link to="/instructors" onClick={()=> setDrawerOpenState(false)}>Instructors</Link>
+                <Link className="btn medium" to="/apply" onClick={()=> setDrawerOpenState(false)}>Apply Now</Link>
             </div>
         </div>
     );
